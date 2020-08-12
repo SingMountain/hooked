13e2fc617240a837d632610c8d72be082a94edfb
邹孟晨 <2078588598@qq.com>
2020-08-12 20:12:30 +0800

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../App.css';
 import Header from './Header';
 import Movie from './Movie';
 import Search from './Search';
-
+import spinne from "../assets/ajax-loader.gif";
 
 const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=e38cefec";
 
@@ -20,19 +20,46 @@ const App = () => {
       });
   }, []);
 
+ 
+
+        
+
   const search = searchValue => {
     setLoading(true);
     setErrorMessage(null);
-    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=e38cefec`)
+    let controller=new AbortController();
+    let signal=controller.signal;
+    let timeoutflag=false;
+    const timeoutPromise = (timeout)=> {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  resolve(timeoutflag=true);
+                }, timeout)
+            })
+        };
+    const fetchPromise=
+      fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=e38cefec`,{signal:signal})
       .then(response => response.json())
       .then(jsonResponse => {
         if (jsonResponse.Response === 'True') {
+          setLoading(false);
           setMovies(jsonResponse.Search);
         } else if (jsonResponse.Error) {
-          setErrorMessage("没找到哦！");
-        }
-        setLoading(false);
-      });
+          setLoading(false);
+          setErrorMessage("没找到哦！");  
+        }  
+      })
+      
+        //超时检测
+        Promise.race([timeoutPromise(5000),fetchPromise]).then(
+          (timeoutflag)=>{
+            if(timeoutflag===true){
+              controller.abort();
+              setLoading(false);
+              setErrorMessage("请求超时！")
+            }
+          });
+     
   }
 
   return (
@@ -42,7 +69,7 @@ const App = () => {
       <p className="App-introduce">搜索你喜欢的电影</p>
       <div className="movies">
         {
-          loading ? (<span>Looding....</span>) :
+          loading ? (<span className="spinne"><img  src={spinne} alt="Loading..." /></span>) :
             errorMessage ? (<div className="errorMessage">{errorMessage}</div>) :
               (movies.map((movie) =>
                 (<Movie key={movie.imdbID} movie={movie} />)
